63ac1f6e084bdc9d3a1c9643e85b94d236f34595
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-14 18:58:35 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,17 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#define MAX_INPUT_LENGTH 1024
-#define MAX_ARG_LENGTH 64
-#define MAX_ARGS 16
-
 /* strtest.c */
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
