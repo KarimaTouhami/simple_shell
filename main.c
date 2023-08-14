2a6e66183c6c60be562390748010137b356222d7
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-14 19:21:13 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int main(void)
+{
+    char input[MAX_INPUT_LENGTH];
+    char *args[MAX_ARGS];
+    char *token;
+
+    while (1)
+    {
+        printf("SimpleShell> ");
+        fgets(input, sizeof(input), stdin);
+
+        /* Remove the newline character from input */
+        input[strcspn(input, "\n")] = '\0';
+
+        /* Tokenize the input */
+        token = strtok(input, " ");
+        int arg_count = 0;
+
+        while (token != NULL)
+        {
+            args[arg_count] = token;
+            arg_count++;
+            token = strtok(NULL, " ");
+        }
+
+        if (arg_count > 0)
+        {
+            args[arg_count] = NULL; /* Set the last element to NULL as required by execvp */
+            if (strcmp(args[0], "exit") == 0)
+            {
+                exit(EXIT_SUCCESS);
+            }
+            execute_command(args[0], args);
+        }
+    }
+
+    return (0);
+}
