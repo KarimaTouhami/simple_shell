d1631a93e81ef52a63d6a64bc35bcfa0356c7c72
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-17 17:43:25 +0100

    Include the allowed functions

--- shell.c
+++ shell.c
@@ -8,48 +8,46 @@
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH];
-	char c;
-	char *args[2];
-	int i = 0;
+    char input[MAX_INPUT_LENGTH];
+    char c;
+    char *args[2];
+    int i = 0;
 
-	while (1)
-	{
-		i = 0;
-		write(STDOUT_FILENO, "$ ", 2);
+    while (1)
+    {
+        i = 0;
+        write(STDOUT_FILENO, "$ ", 2);
 
-		while (read(STDIN_FILENO, &c, 1) > 0)
-		{
-			if (c == '\n' || i == MAX_INPUT_LENGTH - 1)
-				break;
-			input[i++] = c;
-		}
-		input[i] = '\0';
+        while (read(STDIN_FILENO, &c, 1) > 0)
+        {
+            if (c == '\n' || i == MAX_INPUT_LENGTH - 1)
+                break;
+            input[i++] = c;
+        }
+        input[i] = '\0';
 
-		pid_t pid = fork();
+        pid_t pid = fork();
 
-		if (pid == 0)
-		{
-			/* Child process */
-			args[0] = input;
-			args[1] = NULL;
-			execve(input, args, NULL);
+        if (pid == 0)
+        {
+            /* Child process */
+            args[0] = input;
+            args[1] = NULL;
+            execve(input, args, NULL);
 
-			/* If execve fails, print an error message and exit */
-			char error_msg[] = "Error\n";
-			write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-			_exit(EXIT_FAILURE);
-		}
-		else if (pid > 0)
-		{
-			wait(NULL);
-		}
-		else
-		{
-			char error_msg[] = "fork\n";
-			write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-		}
-	}
+            /* If execve fails, print an error message and exit */
+            perror("execve");
+            _exit(EXIT_FAILURE);
+        }
+        else if (pid > 0)
+        {
+            wait(NULL);
+        }
+        else
+        {
+            perror("fork");
+        }
+    }
 
-	return 0;
+    return 0;
 }
