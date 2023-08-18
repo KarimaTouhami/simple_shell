aaa2a3fe1859449f6b90a3a6cb9f880e0103345d
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-18 19:10:44 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,16 +1,21 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
 #define MAX_INPUT_LENGTH 1024
-#define MAX_ARGS 16
+#define MAX_ARGS 64
+#define PATH_MAX_LENGTH 1024
 
-void execute_command(char *command);
+void execute_command(char *command, char **args);
 
 #endif /* SHELL_H */
