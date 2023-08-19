4d5f532c67345dd2bcf3ea0e8b44488f80697ef5
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-19 12:28:00 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -28,39 +28,11 @@ int main(void)
 
         if (pid == 0)
         {
-            int arg_count = 0;
-            args[arg_count++] = strtok(input, " \t\n");
-
-            while ((args[arg_count] = strtok(NULL, " \t\n")) != NULL)
-            {
-                arg_count++;
-                if (arg_count >= MAX_ARGS - 1)
-                    break;
-            }
-            args[arg_count] = NULL;
-
-            /* Get the command's full path */
-            char *cmd_path = get_path(args[0]);
-            if (cmd_path != NULL)
-            {
-                execve(cmd_path, args, NULL);
-                perror("execve");
-                free(cmd_path);
-                _exit(EXIT_FAILURE);
-            }
-            else
-            {
-                char error_message[] = "Command not found: ";
-                write(STDERR_FILENO, error_message, strlen(error_message));
-                write(STDERR_FILENO, args[0], strlen(args[0]));
-                write(STDERR_FILENO, "\n", 1);
-                free(cmd_path);
-                _exit(EXIT_FAILURE);
-            }
+            /* Child process code */
         }
         else if (pid > 0)
         {
-            wait(NULL);
+            /* Parent process code */
         }
         else
         {
