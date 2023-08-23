bfd33ec19b2080eeee960756f9ce0ac5b9970887
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-23 11:58:39 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -10,38 +10,38 @@
 
 int main(int ac, char **av, char **env)
 {
-	char *buf = NULL;
-	size_t buf_size = 0;
-	int n_characters = 0;
-	int shell = 1;
-	(void)ac;
+    char *buf = NULL;
+    size_t buf_size = 0;
+    ssize_t n_characters = 0;
+    int shell = 1;
+    (void)ac;
 
-	signal(SIGINT, sigint_handler);
-	while (shell)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			fflush(stdin);
-			write(STDOUT_FILENO, "$ ", 2);
-		}
-		n_characters = _getline(&buf, &buf_size, STDIN_FILENO);
-		cut_string(buf);
-		if (n_characters == EOF)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
-		if (n_characters > 1)
-			buf[n_characters - 1] = '\0';
-		if (buf[0] == '\n')
-			continue;
-		handle_input(buf, env, av);
-		buf_size = 0;
-		buf = NULL;
-	}
-	free(buf);
-	return (0);
+    signal(SIGINT, sigint_handler);
+    while (shell)
+    {
+        if (isatty(STDIN_FILENO))
+        {
+            fflush(stdin);
+            write(STDOUT_FILENO, "$ ", 2);
+        }
+        n_characters = getline(&buf, &buf_size, stdin);
+        cut_string(buf);
+        if (n_characters == EOF)
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
+        if (n_characters > 1)
+            buf[n_characters - 1] = '\0';
+        if (buf[0] == '\n')
+            continue;
+        handle_input(buf, env, av);
+        buf_size = 0;
+        buf = NULL;
+    }
+    free(buf);
+    return (0);
 }
 
 /**
@@ -49,19 +49,18 @@ int main(int ac, char **av, char **env)
  * @str: string to cut
  * Return: string cut
  */
-
 char *cut_string(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		if (str[i] == '#')
-		{
-			str[i] = '\0';
-			break;
-		}
-		i++;
-	}
-	return (str);
+    while (str[i] != '\0')
+    {
+        if (str[i] == '#')
+        {
+            str[i] = '\0';
+            break;
+        }
+        i++;
+    }
+    return (str);
 }
