cdfbb42a36491387b682ef1071a7b97a3c53500f
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-08-23 09:09:48 +0100

    Update _exit.c

--- _exit.c
+++ _exit.c
@@ -15,3 +15,18 @@ void my_exit(char *input, char *command)
 	free(command);
 	exit(exit_status(0, 0));
 }
+/**
+ * exit_status - Manages the exit status for a given action and new status
+ * @action: The action to perform (1 for set, 0 for get)
+ * @new_status: The new status to set
+ * Return: The current exit status if action is 0,
+ * or the new status if action is 1
+ */
+int exit_status(int action, int new_status)
+{
+	static int status;
+
+	if (action == 1)
+		status = new_status;
+	return (status);
+}
